92d0f50d89d81302943735105e33c4959be80d0b
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-09 12:49:05 +0500

    increment the count as product is created

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -6,19 +6,29 @@ contract Marketplace {
 // a market on blockchain that will buy and sell products and keep track of it (read and write)  
 
 string public name;
+uint public productCount = 0;  // will be incremented as product is added
+mapping(uint => Product) public products;
 
 struct Product{
     uint id;
     string name;
     uint price;
-     address owner;  //who owns the product and will then to buyer who buys the product 
-
+    address owner;  //who owns the product and will then to buyer who buys the product 
+    bool purchased;
 }
 
 constructor() public{
     name = "Gem Store";
 }
 
+function createProduct() public {
+    // check if parametre is correct
+    // create product
+
+    //increment product count
+    productCount++;
+    // trigger an event
+}
 
 
 
