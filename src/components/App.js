c6afeaa49f0d0e73e87f4a664ec6242941dab587
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-10 19:50:34 +0500

    code added to load smart contract from blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,83 +1,61 @@
 import React, { Component } from 'react';
-import Web3 from 'web3'
-import logo from '../logo.png';
+import Web3 from 'web3';
+// import logo from '../logo.png';
 import './App.css';
 import Marketplace from '../abis/Marketplace.json'
 import Navbar from './Navbar'
-import Main from './Main'
 
 class App extends Component {
-
-  async componentWillMount() {
+  async componentDidMount() {
     await this.loadWeb3()
     await this.loadBlockchainData()
   }
 
   async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
-  }
-
-  async loadBlockchainData() {
-    const web3 = window.web3
-    // Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const networkId = await web3.eth.net.getId()
-    const networkData = Marketplace.networks[networkId]
-    if(networkData) {
-      const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
-      this.setState({ marketplace })
-      const productCount = await marketplace.methods.productCount().call()
-      this.setState({ productCount })
-      // Load products
-      for (var i = 1; i <= productCount; i++) {
-        const product = await marketplace.methods.products(i).call()
-        this.setState({
-          products: [...this.state.products, product]
-        })
+      // Modern dapp browsers...
+      if (window.ethereum) {
+          window.web3 = new Web3(window.ethereum); // new connection will be instantiated
+              // Request account access if needed
+              await window.ethereum.enable();
+          }   // Legacy dapp browsers...
+          else if (window.web3) {
+          window.web3 = new Web3(window.web3.currentProvider); // new connection will be instantiated
       }
-      this.setState({ loading: false})
-    } else {
-      window.alert('Marketplace contract not deployed to detected network.')
-    }
+      // Non-dapp browsers...
+      else {
+          window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      }
   }
 
-  constructor(props) {
-    super(props)
-    this.state = {
+  async loadBlockchainData (){
+   const web3 = window.web3
+   //load accounts
+   const accounts = await web3.eth.getAccounts()
+   // console.log(accounts) 
+   this.setState({account: accounts[0]})
+   const networkId = await web3.eth.net.getId()
+   //console.log(networkId) 
+   const networkData = Marketplace.networks[networkId]  //networkId = 5777 or 1337 
+   if (networkData){
+    const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)  //contract loaded from blockchain
+   // console.log(marketplace)
+   this.setState({marketplace})
+   this.setState({loading: false})
+   } else {
+    window.alert('Marketplace contract not deployed to detect network')
+   }
+  
+  }
+
+  constructor(props){
+    super (props)
+    this.state = { 
+      // default values 
       account: '',
       productCount: 0,
       products: [],
       loading: true
     }
-
-    this.createProduct = this.createProduct.bind(this)
-    this.purchaseProduct = this.purchaseProduct.bind(this)
-  }
-
-  createProduct(name, price) {
-    this.setState({ loading: true })
-    this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
-  }
-
-  purchaseProduct(id, price) {
-    this.setState({ loading: true })
-    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
   }
 
   render() {
@@ -87,13 +65,9 @@ class App extends Component {
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex">
-              { this.state.loading
-                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-                : <Main
-                  products={this.state.products}
-                  createProduct={this.createProduct}
-                  purchaseProduct={this.purchaseProduct} />
-              }
+              <div id= "content">
+                <h1>Add Product</h1>
+              </div>
             </main>
           </div>
         </div>
